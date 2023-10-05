9266815b9202041339c540dd8a3c7b786d93ce79
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2023-10-05 16:55:32 +0545

    completed todo

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -4,32 +4,44 @@ import { useTodo } from '../context'
 import { Input } from './Input'
 
 export const AddTodo = () => {
-  const [input, setInput] = useState<string>("")
+  const [input, setInput] = useState<string>('')
   const inputRef = useRef<HTMLInputElement>(null)
+  const { addTodo } = useTodo()
+
+  const [todos, setTodos] = useState<string[]>([])
 
   useEffect(() => {
-    if(inputRef.current){
+    if (inputRef.current) {
       inputRef.current.focus()
     }
-  },[])
+  }, [])
 
   const handleSubmission = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log('form submitted')
+    if (input.trimEnd()!==""){
+      addTodo(input)
+      setInput("")
+      toast.success('Todo added successfully')
+    }else{
+      toast.error('Todo cannot be empty!')
+    }
   }
 
   return (
     <form onSubmit={handleSubmission}>
       <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
-        <input
-        ref={inputRef}
+        <Input
+          ref={inputRef}
           value={input}
           onChange={e => setInput(e.target.value)}
           type="text"
-          className='w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white'
-          placeholder='start typing...'
+          className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white"
+          placeholder="start typing..."
         />
-        <button type='submit' className='px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl'>
+        <button
+          type="submit"
+          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
+        >
           Submit
         </button>
       </div>
